485b2cf8ab8bccffa596bfa8efb7f5a0502ba027
VashuTheGreat <vanshsharma7832@gmail.com>
2026-05-18 22:42:53 +0530

    commented uv run in main

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ load_dotenv()
 import uvicorn
 from api.app import app
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=7860,reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=7860,reload=True)
 
